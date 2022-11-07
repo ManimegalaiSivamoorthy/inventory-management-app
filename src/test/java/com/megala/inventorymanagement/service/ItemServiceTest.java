b88909dd73b91fae0074fb62d3ed77cdf249d1aa
Manimegalai Sivamoorthy <smegala016@gmail.com>
2022-11-07 12:46:02 -0600

    Added more unit test for item service and item dao

---
+++ src/test/java/com/megala/inventorymanagement/service/ItemServiceTest.java
@@ -0,0 +1,90 @@
+package com.megala.inventorymanagement.service;
+
+import com.megala.inventorymanagement.dao.ItemDao;
+import com.megala.inventorymanagement.model.Item;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ItemServiceTest {
+
+    ItemService itemService = new ItemService();
+    ItemDao itemDao = mock(ItemDao.class);
+
+    @BeforeEach
+    void setUp() {
+        itemService.itemDao = itemDao;
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createItem() {
+        Item item = createTestItem();
+        Item createdItem = createTestItem();
+
+        when(itemDao.createItem(item)).thenReturn(createdItem);
+
+        Item resultItem = itemService.createItem(item);
+
+        assertEquals(createdItem, resultItem);
+
+        verify(itemDao).createItem(item);
+    }
+
+    @Test
+    void getItem() {
+        Integer itemId = 2;
+        Item item = createTestItem();
+        item.setId(2);
+
+        when(itemDao.getItem(itemId)).thenReturn(item);
+
+        Item resultItem = itemService.getItem(itemId);
+
+        assertEquals(item, resultItem);
+
+        verify(itemDao).getItem(itemId);
+    }
+
+    @Test
+    void updateItem() {
+        Item item = createTestItem();
+        Integer itemId = 5;
+        item.setId(itemId);
+        Item newUpdatedItem = createTestItem();
+        newUpdatedItem.setId(itemId);
+
+        when(itemDao.updateItem(item)).thenReturn(newUpdatedItem);
+
+        Item resultItem = itemService.updateItem(itemId, item);
+
+        assertEquals(newUpdatedItem, resultItem);
+
+        verify(itemDao).updateItem(item);
+    }
+
+    @Test
+    void removeItem() {
+        Integer itemId = 3;
+
+        itemService.removeItem(itemId);
+
+        verify(itemDao).deleteItem(itemId);
+    }
+
+    Item createTestItem() {
+        Item item = new Item();
+        item.setId(1);
+        item.setDescription("pencil");
+        item.setBrand("HB");
+        item.setCost(0.3);
+        item.setPackageQuantity(12);
+        return item;
+    }
+}
