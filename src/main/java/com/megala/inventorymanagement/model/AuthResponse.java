5eb1a3ea29384baa607e41e3c4026450c682bbf8
Manimegalai Sivamoorthy <smegala016@gmail.com>
2022-12-10 12:24:24 -0600

    demo for feign client

---
+++ src/main/java/com/megala/inventorymanagement/model/AuthResponse.java
@@ -0,0 +1,32 @@
+package com.megala.auth.model;
+
+import java.util.List;
+
+public class AuthResponse {
+    private String userId;
+    private List<Role> validRoles;
+
+    public AuthResponse() {
+    }
+
+    public AuthResponse(String userId, List<Role> validRoles) {
+        this.userId = userId;
+        this.validRoles = validRoles;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public List<Role> getValidRoles() {
+        return validRoles;
+    }
+
+    public void setValidRoles(List<Role> validRoles) {
+        this.validRoles = validRoles;
+    }
+}
---
+++ src/main/java/com/megala/inventorymanagement/model/Role.java
@@ -0,0 +1,5 @@
+package com.megala.auth.model;
+
+public enum Role {
+    READ_ONLY, WRITE, ADMIN, SUPPORT
+}
