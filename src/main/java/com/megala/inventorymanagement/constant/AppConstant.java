0f6bf418ddf732bf001e5138913e02988679a808
Manimegalai Sivamoorthy <smegala016@gmail.com>
2022-12-12 10:14:44 -0600

    Made general walkthrough

--- src/main/java/com/megala/inventorymanagement/constant/AppConstant.java
+++ src/main/java/com/megala/inventorymanagement/constant/AppConstant.java
@@ -5,5 +5,5 @@ import com.megala.inventorymanagement.model.Role;
 import java.util.List;
 
 public class AppConstant {
-    public static List<Role> CREATE_ITEM_ROLES = List.of(Role.WRITE, Role.ADMIN, Role.SUPPORT);
+    public static final List<Role> CREATE_ITEM_ROLES = List.of(Role.WRITE, Role.ADMIN, Role.SUPPORT);
 }
